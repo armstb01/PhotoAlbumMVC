d3a756929be1d45b99c5d21cc82c96256058ba97
armstrong <armstrong@JOKER>
2017-03-01 11:28:03 -0600

    Add project files.

---
+++ PhotoAlbumMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotoAlbumMVC.Startup))]
+namespace PhotoAlbumMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
