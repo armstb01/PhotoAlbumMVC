13b10e27c32073765e6f4c11c81abb4842c37bdd
Armstrong <armstb01@gmail.com>
2017-03-01 20:36:45 -0600

    Add Datatables, JqueryUI, Jquery Create json that retrieves the albums

--- PhotoAlbumMVC/Controllers/HomeController.cs
+++ PhotoAlbumMVC/Controllers/HomeController.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
 
 namespace PhotoAlbumMVC.Controllers
 {
     public class HomeController : Controller
     {
+        public JsonResult AlbumSuggestions(string albumID)
+        {
+            string baseUrl = "https://jsonplaceholder.typicode.com/photos";
+
+            int i;
+
+            if (int.TryParse(albumID.ToString(), out i))
+            {
+                baseUrl = baseUrl + "?albumId=" + albumID.ToString();
+            }
+
+            var json = new WebClient().DownloadString(baseUrl);
+
+            List<PhotoAlbumMVC.Models.Album> album = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Models.Album>>(json);
+
+            return this.Json(
+              new
+              {
+                  sEcho = album.Count(),
+                  iTotalRecords = album.Count(),
+                  iTotalDisplayRecords = album.Count(),
+                  aaData = album.Select(a => new
+                  {
+                      albumId = a.albumID,
+                      id = a.ID,
+                      title = a.title,
+                      url = a.url,
+                      thumbnailUrl = a.thumbnailUrl,
+                  })
+              }
+              , JsonRequestBehavior.AllowGet
+              );
+        }
+
         public ActionResult Index()
         {
             return View();
---
+++ PhotoAlbumMVC/Models/Album.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumMVC.Models
+{
+    //"albumId": 1,
+    //"id": 1,
+    //"title": "accusamus beatae ad facilis cum similique qui sunt",
+    //"url": "http://placehold.it/600/92c952",
+    //"thumbnailUrl": "http://placehold.it/150/30ac17"
+
+    class Album
+    {
+        public int albumID;
+        public int ID;
+        public string title;
+        public string url;
+        public string thumbnailUrl;
+    }
+}
